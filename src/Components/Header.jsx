4883972e35aac3d8b40d165c3ec4181edac5f764
Exsilien <fosterr244@gmail.com>
2022-09-06 16:28:52 +0300

    smth changed

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="http://assets.centralindex.com/W/48/db08933953349172c3dea48efd3ce906.png" alt="logo"/>
+        </header>
+    );
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.1zoom.ru/big2/994/260356-svetik.jpg" alt="#" className="wallpaper"/>
+            </div>
+            <div>
+                ava + discrp
+            </div>
+            <div>
+                my posts
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>post 2</div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
